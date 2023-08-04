8942bf64a1de5bf7456f3b8de9e52ad387f84a61
Maxime Danic <maxime.danic@gmail.com>
2023-08-04 22:07:05 +0300

    🚚 F03: APP > Move routes from main to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,28 @@
-import LoginPage from "./components/pages/login/LoginPage";
+import LoginPage from "./components/pages/login/LoginPage.jsx";
+import OrderPage from "./components/pages/order/OrderPage.jsx";
+import ErrorPage from "./components/pages/error/ErrorPage.jsx";
+
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <LoginPage />,
+  },
+  {
+    path: "/order/:username",
+    element: <OrderPage />,
+  },
+  {
+    path: "*",
+    element: <ErrorPage />,
+  },
+]);
 
 function App() {
   return (
     <>
-      <LoginPage />
+      <RouterProvider router={router} />
     </>
   );
 }
