6717f615f7cbfc1c6ebf6e11c048588cb1785747
Maxime Danic <maxime.danic@gmail.com>
2023-08-06 00:17:24 +0300

    ✨ F04: ORDER > Navbar >Create Profil

--- src/components/pages/order/NavbarRightSide.jsx
+++ src/components/pages/order/NavbarRightSide.jsx
@@ -1,17 +1,31 @@
-import { Link } from "react-router-dom";
+//components
+import Profil from "./Profil";
 
+//Icon
+
+//style
 import styled from "styled-components";
 
 const NavbarRightSide = ({ username }) => {
   return (
     <NavbarRightSideStyled>
-      <h1>Hey {username}</h1>
-      <Link to="/">
-        <button>Log out</button>
-      </Link>
+      {/* <div className="admin-button">Admin button</div> */}
+      <Profil username={username} />
     </NavbarRightSideStyled>
   );
 };
-const NavbarRightSideStyled = styled.div``;
+const NavbarRightSideStyled = styled.div`
+  display: flex;
+  gap: 3.75rem;
+  align-items: center;
+
+  /* .admin-button {
+    background: #000;
+    color: white;
+  } */
+  .profil {
+    background: tomato;
+  }
+`;
 
 export default NavbarRightSide;
