38a45fc2343f4cd884cfd537e33753ee0330b2c3
Maxime Danic <maxime.danic@gmail.com>
2023-08-05 22:10:49 +0300

    🚧 F04: ORDER > layout > add container with nav and main

--- src/components/pages/order/OrderPage.jsx
+++ src/components/pages/order/OrderPage.jsx
@@ -1,15 +1,77 @@
 import { Link, useParams } from "react-router-dom";
 
+//components
+
+//style
+
+import styled from "styled-components";
+import { theme } from "../../../theme";
+import Logo from "../../reusable-ui/Logo";
+
 const OrderPage = () => {
   const { username } = useParams();
 
   return (
-    <>
-      <h1>Bonjour {username}</h1>
-      <Link to="/">
-        <button>log out</button>
-      </Link>
-    </>
+    <OrderPageStyled>
+      <div className="container">
+        <header>
+          <nav>
+            <div className="right-side">
+              logo
+              {/* <Logo /> */}
+            </div>
+            <div className="left-side">
+              <h1>Hey {username}</h1>
+              <Link to="/">
+                <button>Log out</button>
+              </Link>
+            </div>
+          </nav>
+        </header>
+        <main>main</main>
+      </div>
+    </OrderPageStyled>
   );
 };
+
+const OrderPageStyled = styled.div`
+  background-color: orange;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  .container {
+    background-color: red;
+    max-width: 1400px;
+    margin: 0 auto;
+    width: 100%;
+    height: 94vh;
+    border-radius: ${theme.borderRadius.extraRound};
+    overflow: hidden;
+    display: flex;
+    flex-direction: column;
+
+    header {
+      background-color: white;
+      width: 100%;
+      padding: 2rem 1.5rem;
+
+      nav {
+        display: flex;
+        justify-content: space-between;
+
+        .right-side {
+          /* transform: scale(0.4); */
+        }
+      }
+    }
+
+    main {
+      background-color: green;
+      flex: 1;
+    }
+  }
+`;
+
 export default OrderPage;
