e42714006971d0b92af0a5cc6fb6c9f4385be531
Maxime Danic <maxime.danic@gmail.com>
2023-08-06 17:32:27 +0300

    🚧 F05: Main > Update Card layout

--- src/components/pages/order/Main.jsx
+++ src/components/pages/order/Main.jsx
@@ -6,7 +6,10 @@ const Main = () => {
     <MainStyled>
       <div className="card">
         card
-        <div className="image">image</div>
+        <div className="container-image">
+          container image
+          <div className="image">image</div>
+        </div>
         <div className="title">title</div>
         <div className="container-price-add">
           <div className="price">price</div>
@@ -23,26 +26,44 @@ const MainStyled = styled.main`
   box-shadow: 0px 8px 20px 8px rgba(0, 0, 0, 0.2) inset;
 
   .card {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+
     background: tomato;
-    width: 100%;
-    height: 100%;
     max-width: 240px;
     max-height: 330px;
-    .image {
-      background: green;
+    height: 100%;
+    padding: 1.25rem;
+    .container-image {
+      background: red;
+      width: 200px;
+      height: 145px;
+      overflow: hidden;
+
+      .image {
+        background: green;
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+      }
     }
 
     .title {
       background: blueviolet;
     }
     .container-price-add {
+      display: flex;
+      justify-content: space-between;
       background: yellowgreen;
 
       .price {
         background: orange;
+        width: 100%;
       }
       .add-button {
         background: yellow;
+        width: 100%;
       }
     }
   }
