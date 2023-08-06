8bfd48919d8a3d16ad42735a8be7df17c2ad394b
Maxime Danic <maxime.danic@gmail.com>
2023-08-06 17:16:56 +0300

    🚧 F05: Main > Create Card layout

--- src/components/pages/order/Main.jsx
+++ src/components/pages/order/Main.jsx
@@ -2,12 +2,49 @@ import styled from "styled-components";
 import { theme } from "../../../theme";
 
 const Main = () => {
-  return <MainStyled>Main</MainStyled>;
+  return (
+    <MainStyled>
+      <div className="card">
+        card
+        <div className="image">image</div>
+        <div className="title">title</div>
+        <div className="container-price-add">
+          <div className="price">price</div>
+          <div className="add-button">Add</div>
+        </div>
+      </div>
+    </MainStyled>
+  );
 };
 
 const MainStyled = styled.main`
   flex: 1;
   background-color: ${theme.colors.background_white};
   box-shadow: 0px 8px 20px 8px rgba(0, 0, 0, 0.2) inset;
+
+  .card {
+    background: tomato;
+    width: 100%;
+    height: 100%;
+    max-width: 240px;
+    max-height: 330px;
+    .image {
+      background: green;
+    }
+
+    .title {
+      background: blueviolet;
+    }
+    .container-price-add {
+      background: yellowgreen;
+
+      .price {
+        background: orange;
+      }
+      .add-button {
+        background: yellow;
+      }
+    }
+  }
 `;
 export default Main;
