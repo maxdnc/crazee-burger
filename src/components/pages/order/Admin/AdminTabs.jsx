9e63497801374311d23e8d335ddc98e858e150ad
Maxime Danic <maxime.danic@gmail.com>
2023-08-14 11:58:09 +0300

    🚧 F07: ADMIN > AdminTabs > Update label and icon's tab

--- src/components/pages/order/Admin/AdminTabs.jsx
+++ src/components/pages/order/Admin/AdminTabs.jsx
@@ -1,13 +1,19 @@
 import styled from "styled-components";
-
+//component
 import Tab from "../../../reusable-ui/Tab";
+
+// Icon
+
+import { FiChevronDown } from "react-icons/fi";
+import { AiOutlinePlus } from "react-icons/ai";
+import { MdModeEditOutline } from "react-icons/md";
 
 const AdminTabs = () => {
   return (
     <AdminTabsStyled>
-      <Tab />
-      <Tab />
-      <Tab />
+      <Tab Icon={<FiChevronDown />} />
+      <Tab label={"Ajouter un produit"} Icon={<AiOutlinePlus />} />
+      <Tab label={"Modifier un produit"} Icon={<MdModeEditOutline />} />
     </AdminTabsStyled>
   );
 };
