7cd2933d9cb15fe036d78fd746feba06dae21c2f
Maxime Danic <maxime.danic@gmail.com>
2023-08-14 11:51:14 +0300

    🚧 F07: TAB > Update Tab layout

--- src/components/reusable-ui/Tab.jsx
+++ src/components/reusable-ui/Tab.jsx
@@ -1,4 +1,19 @@
-const Tab = () => {
-  return <button>Tab</button>;
+import styled from "styled-components";
+
+const Tab = ({ Icon, label }) => {
+  return (
+    <TabStyled>
+      {Icon && <span>{Icon}</span>}
+      {label && <span>{label}</span>}
+    </TabStyled>
+  );
 };
+
+const TabStyled = styled.button`
+  display: inline-flex;
+  align-items: center;
+  gap: 0.8rem;
+  padding: 0.8rem 1.375rem;
+`;
+
 export default Tab;
