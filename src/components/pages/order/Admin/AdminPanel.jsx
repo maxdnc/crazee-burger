98fe269f2584eff1e23acb12d5bd6ce2de6a1409
Maxime Danic <maxime.danic@gmail.com>
2023-08-15 11:15:58 +0300

    🚧 F07: ADMIN > ADMINPANEL > Add openPanel state

--- src/components/pages/order/Admin/AdminPanel.jsx
+++ src/components/pages/order/Admin/AdminPanel.jsx
@@ -1,10 +1,20 @@
+import { useState } from "react";
 import styled from "styled-components";
 
 const AdminPanel = () => {
-  return <AdminPanelStyled>AdminPanel</AdminPanelStyled>;
+  const [openPanel, setOpenPanel] = useState(false);
+
+  return (
+    <AdminPanelStyled className={openPanel ? "open-panel" : ""}>
+      AdminPanel
+    </AdminPanelStyled>
+  );
 };
 
 const AdminPanelStyled = styled.div`
   background-color: tomato;
+  &.open-panel {
+    height: 250px;
+  }
 `;
 export default AdminPanel;
