6a0fcf111a035787bb0d00d59fb154b53c9da9e3
Maxime Danic <maxime.danic@gmail.com>
2023-08-19 01:59:03 +0300

    ✨ F07: ADMIN > PANEL > Add scalable implementation

--- src/components/pages/order/Admin/AdminPanel.jsx
+++ src/components/pages/order/Admin/AdminPanel.jsx
@@ -1,20 +1,19 @@
-import { useState } from "react";
+import { useContext } from "react";
+import OrderContext from "../../../../context/OrderContext";
 import styled from "styled-components";
+import { getTabSelected, tabsConfig } from "./tabsConfig";
 
 const AdminPanel = () => {
-  const [openPanel, setOpenPanel] = useState(false);
+  const { currentTabSelected } = useContext(OrderContext);
+  const tabs = tabsConfig;
 
-  return (
-    <AdminPanelStyled className={openPanel ? "open-panel" : ""}>
-      AdminPanel
-    </AdminPanelStyled>
-  );
+  const tabSelected = getTabSelected(tabs, currentTabSelected);
+
+  return <AdminPanelStyled>{tabSelected.label}</AdminPanelStyled>;
 };
 
 const AdminPanelStyled = styled.div`
   background-color: tomato;
-  &.open-panel {
-    height: 250px;
-  }
+  height: 250px;
 `;
 export default AdminPanel;
