56af9e6ef4dd5a97c444652a1c2202a9cbba53b8
Maxime Danic <maxime.danic@gmail.com>
2023-08-22 11:11:22 +0300

    💄 F08: PRIMARYBUTTON > Add extraStyles version

--- src/components/reusable-ui/PrimaryButton.jsx
+++ src/components/reusable-ui/PrimaryButton.jsx
@@ -1,9 +1,9 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 import { theme } from "../../theme";
 
-const PrimaryButton = ({ label, Icon, className }) => {
+const PrimaryButton = ({ label, Icon, className, version = "normal" }) => {
   return (
-    <PrimaryButtonStyled className={className}>
+    <PrimaryButtonStyled className={className} version={version}>
       <span>{label}</span>
       {Icon && <span> {Icon}</span>}
     </PrimaryButtonStyled>
@@ -12,24 +12,16 @@ const PrimaryButton = ({ label, Icon, className }) => {
 
 const PrimaryButtonStyled = styled.button`
   width: 100%;
-  background-color: ${theme.colors.primary};
-  border: 1px solid ${theme.colors.primary};
   border-radius: ${theme.borderRadius.round};
-  padding-block: 1rem;
-  color: ${theme.colors.white};
-  font-size: ${theme.fonts.P0};
   font-weight: ${theme.weights.bold};
-  display: inline-flex;
-  justify-content: center;
-  align-items: center;
   display: inline-flex;
   justify-content: center;
   align-items: center;
   gap: 0.75rem;
   line-height: small;
-
   cursor: pointer;
   transition: all 150ms ease-out;
+
   &:hover {
     background-color: ${theme.colors.white};
     color: ${theme.colors.primary};
@@ -42,6 +34,34 @@ const PrimaryButtonStyled = styled.button`
     background-color: ${theme.colors.primary};
     color: ${theme.colors.white};
   }
+
+  ${({ version }) => extraStyles[version]}
 `;
 
+const extraStyleNormal = css`
+  background-color: ${theme.colors.primary};
+  border: 1px solid ${theme.colors.primary};
+  padding-block: 1rem;
+  color: ${theme.colors.white};
+  font-size: ${theme.fonts.P0};
+`;
+
+const extraStyleSuccess = css`
+  background-color: ${theme.colors.success};
+  border: 1px solid ${theme.colors.primary};
+  padding-block: 0.5rem;
+  border-color: ${theme.colors.success};
+  font-size: ${theme.fonts.XS};
+  color: ${theme.colors.white};
+
+  &:hover {
+    background-color: ${theme.colors.white};
+    color: ${theme.colors.success};
+  }
+`;
+
+const extraStyles = {
+  normal: extraStyleNormal,
+  success: extraStyleSuccess,
+};
 export default PrimaryButton;
