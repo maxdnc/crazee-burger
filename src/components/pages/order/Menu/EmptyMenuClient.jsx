14a345d5f7115b676761a7a1fd90cd243efa8e90
Maxime Danic <maxime.danic@gmail.com>
2023-08-22 12:08:53 +0300

    💄 F08: MENU > Add EmptyMenu styles ( admin and client mode )

--- src/components/pages/order/Menu/EmptyMenuClient.jsx
+++ src/components/pages/order/Menu/EmptyMenuClient.jsx
@@ -1,4 +1,44 @@
+import styled from "styled-components";
+import { theme } from "../../../../theme";
+
 const EmptyMenuClient = () => {
-  return <div>EmptyMenuClient</div>;
+  return (
+    <EmptyMenuClientStyled>
+      <p className="title">Victime de notre succès ! :D</p>
+      <p className="description">
+        De nouvelles recettes sont en cours de préparation.
+      </p>
+      <p className="description">À très vite !</p>
+    </EmptyMenuClientStyled>
+  );
 };
+
+const EmptyMenuClientStyled = styled.div`
+  background-color: ${theme.colors.background_white};
+  box-shadow: ${theme.shadows.strong};
+  border-bottom-right-radius: ${theme.borderRadius.extraRound};
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 1.25rem;
+  height: 100%;
+  padding-bottom: 5%;
+
+  .title,
+  .description {
+    text-align: center;
+    font-family: ${theme.fontsFamily.amatic};
+    color: ${theme.colors.greyBlue};
+  }
+
+  .title {
+    font-size: ${theme.fonts.P4};
+    font-weight: ${theme.weights.semiBold};
+  }
+
+  .description {
+    font-size: ${theme.fonts.P4};
+  }
+`;
 export default EmptyMenuClient;
