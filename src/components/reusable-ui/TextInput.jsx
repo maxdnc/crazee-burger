1b7486de9eb4a7c9fe3db77ca3ec2ee8c2419960
Maxime Danic <maxime.danic@gmail.com>
2023-08-22 11:10:26 +0300

    💄 F08: TEXTINPUT > Add extraStyles version

--- src/components/reusable-ui/TextInput.jsx
+++ src/components/reusable-ui/TextInput.jsx
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 import { theme } from "../../theme";
 
 const TextInput = ({
@@ -8,10 +8,11 @@ const TextInput = ({
   label,
   id,
   className,
+  version = "normal",
   ...extraProps
 }) => {
   return (
-    <TextInputStyled className={className}>
+    <TextInputStyled className={className} version={version}>
       <span>{Icon && Icon}</span>
       {label && (
         <label hidden htmlFor={id}>
@@ -30,28 +31,56 @@ const TextInput = ({
 };
 
 const TextInputStyled = styled.div`
-  width: 100%;
-  background-color: ${theme.colors.white};
   border-radius: ${theme.borderRadius.round};
-  padding: 1rem 1.5rem;
+  width: 100%;
   display: inline-flex;
   align-items: center;
   gap: 1rem;
-
-  span {
-    color: ${theme.colors.greyDark};
-  }
 
   input {
     width: 100%;
     border: none;
     background: none;
+    text-overflow: ellipsis;
+  }
+  ${({ version }) => extraStyles[version]}
+`;
 
+const extraStyleNormal = css`
+  background-color: ${theme.colors.white};
+  color: ${theme.colors.greySemiDark};
+  padding: 1rem 1.5rem;
+  input {
+    color: ${theme.colors.dark};
+    &::placeholder {
+      color: ${theme.colors.greySemiMedium};
+      font-size: ${theme.fonts.P0};
+    }
+  }
+  span {
+    color: ${theme.colors.greyDark};
+  }
+`;
+
+const extraStyleMinimalist = css`
+  background-color: ${theme.colors.background_white};
+  padding: 0 1.5rem;
+  input {
+    background: ${theme.colors.background_white};
+    color: ${theme.colors.dark};
     &::placeholder {
       color: ${theme.colors.greyMedium};
       font-size: ${theme.fonts.P0};
     }
   }
+  span {
+    color: ${theme.colors.greyBlue};
+  }
 `;
+
+const extraStyles = {
+  normal: extraStyleNormal,
+  minimalist: extraStyleMinimalist,
+};
 
 export default TextInput;
