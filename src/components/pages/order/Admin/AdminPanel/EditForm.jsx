333cfd27795f49ad508802f36a6496640ea0535b
Maxime Danic <maxime.danic@gmail.com>
2023-08-26 14:12:45 +0300

    ♻️ F09: ADMIN > Create reusable Form  with children to make it more scalable

--- src/components/pages/order/Admin/AdminPanel/EditForm.jsx
+++ src/components/pages/order/Admin/AdminPanel/EditForm.jsx
@@ -1,13 +1,7 @@
-import { useContext, useState } from "react";
-import { styled } from "styled-components";
-
-//config
-import { getTextInputsConfig } from "./textInputsConfig";
+import { useContext } from "react";
 //component
-import TextInput from "../../../../reusable-ui/TextInput";
-import ImagePreview from "./ImagePreview";
 import EditInfoMessage from "./EditInfoMessage";
-
+import Form from "./Form";
 //context
 import OrderContext from "../../../../../context/OrderContext";
 
@@ -19,10 +13,6 @@ const EditForm = () => {
     titleEditRef,
   } = useContext(OrderContext);
 
-  const [isSubmitted, setIsSubmitted] = useState(false);
-
-  const textInputs = getTextInputsConfig(selectedProduct);
-
   const handleChange = (event) => {
     const { name, value } = event.target;
     const productBeingUpdated = { ...selectedProduct, [name]: value };
@@ -32,48 +22,16 @@ const EditForm = () => {
   };
 
   return (
-    <EditFormStyled>
-      <ImagePreview
-        imageSource={selectedProduct.imageSource}
-        title={selectedProduct.title}
-      />
-      <div className="container-inputs">
-        {textInputs.map((product) => (
-          <TextInput
-            {...product}
-            key={product.name}
-            onChange={handleChange}
-            version="minimalist"
-            ref={product.name === "title" ? titleEditRef : null}
-          />
-        ))}
-      </div>
-      <div className="submit">
+    <Form
+      productData={selectedProduct}
+      onChange={handleChange}
+      ref={titleEditRef}
+    >
+      <>
         <EditInfoMessage />
-      </div>
-    </EditFormStyled>
+      </>
+    </Form>
   );
 };
 
-const EditFormStyled = styled.form`
-  display: grid;
-  grid-template-columns: 1fr 3fr;
-  grid-template-rows: repeat(4, 1fr);
-  height: 100%;
-  width: 70%;
-  gap: 8px 20px;
-
-  .container-inputs {
-    display: grid;
-    row-gap: 0.5rem;
-    width: 100%;
-    grid-area: 1 / 2 / 4 / 3;
-  }
-
-  .submit {
-    grid-area: 4 / 2 / 5 / 3;
-    display: flex;
-    align-items: center;
-  }
-`;
 export default EditForm;
---
+++ src/components/pages/order/Admin/AdminPanel/Form.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+//style
+import { styled } from "styled-components";
+//config
+import { getTextInputsConfig } from "./textInputsConfig";
+//component
+import TextInput from "../../../../reusable-ui/TextInput";
+import ImagePreview from "./ImagePreview";
+
+const Form = React.forwardRef(
+  ({ productData, onChange, onSubmit, children }, ref) => {
+    const textInputs = getTextInputsConfig(productData);
+
+    return (
+      <FormStyled onSubmit={onSubmit}>
+        <ImagePreview
+          imageSource={productData.imageSource}
+          title={productData.title}
+        />
+        <div className="container-inputs">
+          {textInputs.map((product) => (
+            <TextInput
+              {...product}
+              key={product.name}
+              onChange={onChange}
+              version="minimalist"
+              ref={ref && product.name === "title" ? ref : null}
+            />
+          ))}
+        </div>
+        <div className="form-footer">{children}</div>
+      </FormStyled>
+    );
+  }
+);
+
+const FormStyled = styled.form`
+  display: grid;
+  grid-template-columns: 1fr 3fr;
+  grid-template-rows: repeat(4, 1fr);
+  height: 100%;
+  width: 70%;
+  gap: 8px 20px;
+
+  .container-inputs {
+    display: grid;
+    row-gap: 0.5rem;
+    width: 100%;
+    grid-area: 1 / 2 / 4 / 3;
+  }
+
+  .form-footer {
+    grid-area: 4 / 2 / 5 / 3;
+  }
+`;
+export default Form;
