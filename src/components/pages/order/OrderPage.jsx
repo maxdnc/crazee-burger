6603fb5012ad64cd9612addc652aefb47b56f172
Maxime Danic <maxime.danic@gmail.com>
2023-08-27 15:23:59 +0300

    ♻️ F10: ORDER > OrderPage > Add useMenuData.js

--- src/components/pages/order/OrderPage.jsx
+++ src/components/pages/order/OrderPage.jsx
@@ -3,53 +3,30 @@ import { useState, useRef } from "react";
 //components
 import { Navbar } from "./Navbar/Navbar.jsx";
 import Main from "./Main/Main.jsx";
-//data
-import { fakeMenu } from "../../../fakeData/fakeMenu";
-import { EMPTY_PRODUCT } from "../../../enums/product";
 
 //style
 import styled from "styled-components";
 
 import AdminContext from "../../../context/OrderContext.js";
-
-//utils
-import { deepCopy } from "../../../utils/array";
+import { useMenuData } from "../../../hooks/useMenuData.js";
 
 const OrderPage = () => {
   const [isModeAdmin, setIsModeAdmin] = useState(false);
   const [isCollapsed, setIsCollapsed] = useState(false);
   const [currentTabSelected, setCurrentTabSelected] = useState("add");
-  const [menuData, setMenuData] = useState(fakeMenu.LARGE);
-  const [newProduct, setNewProduct] = useState(EMPTY_PRODUCT);
-  const [selectedProduct, setSelectedProduct] = useState(EMPTY_PRODUCT);
   const titleEditRef = useRef();
 
-  const handleAddToMenu = (newProduct) => {
-    const menuDataCopy = deepCopy(menuData);
-
-    const menuDataUpdated = [newProduct, ...menuDataCopy];
-    setMenuData(menuDataUpdated);
-  };
-
-  const handleDeleteToMenu = (idProductToDelete) => {
-    const menuDataCopy = deepCopy(menuData);
-
-    const menuUpdated = menuDataCopy.filter(
-      (product) => product.id !== idProductToDelete
-    );
-    setMenuData(menuUpdated);
-  };
-
-  const handleEditToMenu = (productBeingEdited) => {
-    const menuDataCopy = deepCopy(menuData);
-    const indexOfProducToEdit = menuData.indexOf(selectedProduct);
-    menuDataCopy[indexOfProducToEdit] = productBeingEdited;
-    setMenuData(menuDataCopy);
-  };
-
-  const resetMenu = () => {
-    setMenuData(fakeMenu.LARGE);
-  };
+  const {
+    menuData,
+    handleAddToMenu,
+    handleDeleteToMenu,
+    handleEditToMenu,
+    resetMenu,
+    selectedProduct,
+    setSelectedProduct,
+    newProduct,
+    setNewProduct,
+  } = useMenuData();
 
   const orderContextValue = {
     isModeAdmin,
@@ -59,14 +36,13 @@ const OrderPage = () => {
     currentTabSelected,
     setCurrentTabSelected,
     menuData,
-    setMenuData,
+    selectedProduct,
+    setSelectedProduct,
     handleAddToMenu,
     handleDeleteToMenu,
     resetMenu,
     newProduct,
     setNewProduct,
-    selectedProduct,
-    setSelectedProduct,
     handleEditToMenu,
     titleEditRef,
   };
