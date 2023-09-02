55a1932a4508f3cc7edfde51c44f74c774a0d8f3
Maxime Danic <maxime.danic@gmail.com>
2023-09-02 12:25:19 +0300

    🚧 F12: BASKET > ProductsSelection > Add handleClick

--- src/components/pages/order/Main/Basket/BasketBody/ProductsSelection.jsx
+++ src/components/pages/order/Main/Basket/BasketBody/ProductsSelection.jsx
@@ -3,6 +3,10 @@ import BasketCard from "./BasketCard/BasketCard";
 import OrderContext from "../../../../../../context/OrderContext";
 import { useContext } from "react";
 import { formatPrice } from "../../../../../../utils/maths";
+import { checkIfProductIsSelected } from "../../../../../../utils/helper";
+import { isProductSelected } from "../../../../../../utils/isProductSelected";
+import { findInArray } from "../../../../../../utils/array";
+import { EMPTY_PRODUCT } from "../../../../../../enums/product";
 
 const ProductsSelection = () => {
   const {
@@ -10,24 +14,64 @@ const ProductsSelection = () => {
     handleDeleteToBasket,
     handleIncrementQuantityProduct,
     handleDecrementQuantityProduct,
+    isModeAdmin,
+    selectedProduct,
+    setSelectedProduct,
+    setIsCollapsed,
+    setCurrentTabSelected,
+    titleEditRef,
   } = useContext(OrderContext);
 
+  const handleClick = async (idSelectedProduct) => {
+    const isSelectedProductSame =
+      isProductSelected(selectedProduct) &&
+      selectedProduct.id === idSelectedProduct;
+
+    if (isSelectedProductSame) {
+      setSelectedProduct(EMPTY_PRODUCT);
+    } else {
+      const selectedProduct = findInArray(basketProducts, idSelectedProduct);
+
+      await setIsCollapsed(false);
+      await setSelectedProduct(selectedProduct);
+      await setCurrentTabSelected("edit");
+      titleEditRef.current.focus();
+    }
+  };
+
+  const handleDelete = (event, idBasketProduct) => {
+    event.stopPropagation();
+    handleDeleteToBasket(idBasketProduct);
+  };
+
+  const handleIncrement = (event, idBasketProduct) => {
+    event.stopPropagation();
+    handleIncrementQuantityProduct(idBasketProduct);
+  };
+
+  const handleDecrement = (event, idBasketProduct) => {
+    event.stopPropagation();
+    handleDecrementQuantityProduct(idBasketProduct);
+  };
   return (
     <ProductsSelectionStyled>
       {basketProducts.map(({ id, title, price, imageSource, quantity }) => {
         return (
           <li key={id}>
             <BasketCard
+              isSelected={checkIfProductIsSelected(id, selectedProduct.id)}
+              isHoverable={isModeAdmin}
+              onClick={isModeAdmin ? () => handleClick(id) : null}
               title={title}
               price={formatPrice(price)}
               imageSource={imageSource}
-              deleteProduct={() => handleDeleteToBasket(id)}
+              deleteProduct={(event) => handleDelete(event, id)}
               quantity={quantity}
-              incrementQuantity={() => {
-                handleIncrementQuantityProduct(id);
+              incrementQuantity={(event) => {
+                handleIncrement(event, id);
               }}
-              decrementQuantity={() => {
-                handleDecrementQuantityProduct(id);
+              decrementQuantity={(event) => {
+                handleDecrement(event, id);
               }}
             />
           </li>
