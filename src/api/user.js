ca307a2fb9f463e2e2bb48df2e9c9c7e468d6818
Maxime Danic <maxime.danic@gmail.com>
2023-09-05 13:07:51 +0300

    ✨ F14: API > user > add getter and setter

---
+++ src/api/user.js
@@ -0,0 +1,22 @@
+import { doc, getDoc, setDoc } from "firebase/firestore";
+import { db } from "./firebase.config";
+import { fakeMenu } from "../fakeData/fakeMenu";
+
+export const getUser = async (userId) => {
+  const docRef = doc(db, "users", userId);
+  const docSnapShot = await getDoc(docRef);
+  if (docSnapShot.exists()) {
+    const userReceived = docSnapShot.data();
+    return userReceived;
+  }
+};
+
+export const createUser = (userId) => {
+  const docRef = doc(db, "users", userId);
+  const infoUser = {
+    username: userId,
+    menu: fakeMenu.LARGE,
+  };
+
+  setDoc(docRef, infoUser);
+};
