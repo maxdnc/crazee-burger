c75fca4c775c800780c351f66961a80981615cc2
Maxime Danic <maxime.danic@gmail.com>
2023-09-10 15:00:57 +0300

    ♻️ F15: BASKET > Extract basketAnimation

--- src/components/pages/order/Main/Basket/BasketBody/ProductsSelection.jsx
+++ src/components/pages/order/Main/Basket/BasketBody/ProductsSelection.jsx
@@ -2,6 +2,9 @@ import React, { useContext, useRef } from "react";
 //Style
 import styled from "styled-components";
 import { devices } from "../../../../../../enums/devices";
+//animation
+import { TransitionGroup, CSSTransition } from "react-transition-group";
+import { basketAnimation } from "../../../../../../theme/animation";
 //component
 import BasketCard from "./BasketCard/BasketCard";
 //context
@@ -12,9 +15,6 @@ import {
   checkIfSameProductIsSelected,
   findInArrayById,
 } from "../../../../../../utils/array";
-//animation
-
-import { TransitionGroup, CSSTransition } from "react-transition-group";
 
 const ProductsSelection = () => {
   const {
@@ -116,61 +116,6 @@ const ProductsSelectionStyled = styled.ul`
     }
   }
 
-  .animated-basket-card-appear {
-    .basket-card {
-      opacity: 0;
-      transform: translateY(-10%);
-    }
-  }
-
-  .animated-basket-card-enter {
-    .basket-card {
-      opacity: 0;
-      transform: translateY(-10%);
-      max-height: 0px;
-      margin-bottom: 0;
-      padding: 0 0;
-      transform: translateY(-10%);
-      max-height: 0px;
-      margin-bottom: 0;
-      padding: 0 0;
-    }
-  }
-
-  .animated-basket-card-appear-active,
-  .animated-basket-card-enter-active {
-    .basket-card {
-      transform: translateY(0);
-      transform: translateY(0);
-      opacity: 1;
-      transition: all 0.2s ease-out;
-      max-height: 86px;
-      margin-bottom: 1.25rem;
-      padding: 0.5rem 1rem;
-    }
-  }
-
-  .animated-basket-card-exit {
-    .basket-card {
-      transform: translateX(0);
-      opacity: 1;
-      max-height: 86px;
-      max-height: 86px;
-    }
-  }
-
-  .animated-basket-card-exit-active {
-    .basket-card {
-      transform: translateX(-100%);
-      opacity: 0;
-      transition: all 0.2s ease-in;
-      max-height: 0px;
-      margin-bottom: 0;
-      padding: 0 0;
-      max-height: 0px;
-      margin-bottom: 0;
-      padding: 0 0;
-    }
-  }
+  ${basketAnimation}
 `;
 export default ProductsSelection;
